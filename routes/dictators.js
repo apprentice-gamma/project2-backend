0e85b1fbb58ffc42e6b67f08e9aa2b9999bd58a7
bitingteresa <teresa@scientificlee.com>
2015-04-13 17:16:08 -0400

    add routes for get for dictator as well as model and adjusted server.js

--- routes/dictators.js
+++ routes/dictators.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Dictator = require('../models/dictator.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	Dictator.find(function (err, dictator) {
+		if(err) return next(err);
+		res.json(dictator);
+	});
 });
+
+
 
 module.exports = router;
